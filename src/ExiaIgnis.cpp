ac3062264ec6cce4c291d1c34fe0c80668a54f94
Naophis <nao1288stusj@gmail.com>
2026-04-14 23:34:48 +0900

    Refactor ExiaIgnis.cpp to remove unused SPI/I2C/UART defines and add GPIO button handling; create define.hpp for hardware configurations

--- src/ExiaIgnis.cpp
+++ src/ExiaIgnis.cpp
@@ -7,21 +7,7 @@
 #include "hardware/interp.h"
 #include "hardware/uart.h"
 
-// SPI Defines
-// We are going to use SPI 0, and allocate it to the following GPIO pins
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define SPI_PORT spi0
-#define PIN_MISO 16
-#define PIN_CS   17
-#define PIN_SCK  18
-#define PIN_MOSI 19
-
-// I2C defines
-// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
+#include "define.hpp"
 
 // Data will be copied from src to dst
 const char src[] = "Hello, world! (from DMA)";
@@ -41,21 +27,15 @@ void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
 }
 
 
-// UART defines
-// By default the stdout UART is `uart0`, so we will use the second one
-#define UART_ID uart1
-#define BAUD_RATE 115200
-
-// Use pins 4 and 5 for UART1
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define UART_TX_PIN 4
-#define UART_RX_PIN 5
-
-
 
 int main()
 {
     stdio_init_all();
+
+    // Tactile switch input with pull-up (active low)
+    gpio_init(BTN_PIN);
+    gpio_set_dir(BTN_PIN, GPIO_IN);
+    gpio_pull_up(BTN_PIN);
 
     // SPI initialisation. This example will use SPI at 1MHz.
     // spi_init(SPI_PORT, 1000*1000);
@@ -145,7 +125,8 @@ int main()
     // // For more examples of UART use see https://github.com/raspberrypi/pico-examples/tree/master/uart
 
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        bool btn_pressed = !gpio_get(BTN_PIN); // active low: LOW=pressed
+        printf("BTN(GPIO4): %s\n", btn_pressed ? "PRESSED" : "RELEASED");
+        sleep_ms(100);
     }
 }
