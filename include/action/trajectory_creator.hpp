8cee27730597010200db8be4862eeb8631ef7b88
Naophis <nao1288stusj@gmail.com>
2026-05-04 05:20:37 +0900

    Add TrajectoryCreator class for trajectory generation and manipulation
    - Implemented methods to determine turn types and directions based on turn numbers.
    - Added functionality to calculate target angles, radii, front and back distances for various turn types.
    - Developed methods for executing trajectory generation with slalom and straight movements.
    - Introduced helper functions for managing trajectory points and calculating distances.
    - Initialized trajectory data for different turn types in the constructor.

--- include/action/trajectory_creator.hpp
+++ include/action/trajectory_creator.hpp
@@ -1,102 +1,78 @@
 #pragma once
-// Astraea TrajectoryCreator から移植 (FreeRTOS/ESP-IDF 依存を除去)
-// 使用メソッドのみ実装: get_turn_dir / get_turn_type / get_next_dir
+#ifndef TRAJECTORY_CREATOR_H
+#define TRAJECTORY_CREATOR_H
 
-#include "planning/astraea_types.hpp"
+#include <cstdint>
+#include "maze_solver.hpp"
+#include "stdio.h"
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+using namespace std;
 
 class TrajectoryCreator {
+private:
+  float get_run_dir(Direction dir, float ang);
+  float get_turn_tgt_ang(TurnType type);
+  float get_turn_rad(TurnType type);
+  float get_front_dist(TurnType type, bool dia);
+  float get_back_dist(TurnType type, bool dia);
+  float get_slalom_etn(TurnType type, bool dia);
+  float get_slalom_time(TurnType type, bool dia);
+
+  void fix_pos(ego_odom_t &ego, TurnType type, TurnDirection turn_dir,
+               trajectory_point_t &trj_ele);
+  float Et2(float t, float s, float N);
+  void run_straight(vector<trajectory_point_t> &trajectory, float dist,
+                    trajectory_point_t &trj_ele, ego_odom_t &ego,
+                    run_param_t &param, char type);
+  void slalom(vector<trajectory_point_t> &trajectory, int turn_num,
+              trajectory_point_t &trj_ele, ego_odom_t &ego, run_param_t &param,
+              bool dia);
+  float cell_size = 180;
+  float half_cell_size = 90;
+  float get_dist_pt(trajectory_point_t &from, trajectory_point_t &to);
+  void get_next_from_pt(trajectory_point_t &from, trajectory_point_t &to);
+  void get_next_from_pt_x(trajectory_point_t &from, trajectory_point_t &to);
+  void get_next_from_pt_y(trajectory_point_t &from, trajectory_point_t &to);
+
+  void get_next_from_theta_x(trajectory_point_t &from, trajectory_point_t &to,
+                             float next_x);
+  void get_next_from_theta_y(trajectory_point_t &from, trajectory_point_t &to,
+                             float next_y);
+
+  base_trajectory_pattern_t zipped_trj;
+
+  void slalom2(vector<trajectory_point_t> &trajectory, int turn_num,
+               trajectory_point_t &trj_ele, ego_odom_t &ego, run_param_t &param,
+               bool dia);
+
+  void repaint_slalom(vector<trajectory_point_t> &trajectory,
+                      vector<trajectory_point_t> &base_trj,
+                      trajectory_point_t &trj_ele, ego_odom_t &ego,
+                      run_param_t &param, TurnDirection turn_dir,
+                      TurnType type);
+
 public:
-    TurnDirection get_turn_dir(int turn_num) {
-        if (turn_num == 255 || turn_num == 0)
-            return TurnDirection::End;
-        return (turn_num & 0x01) == 0x01 ? TurnDirection::Right : TurnDirection::Left;
-    }
+  TrajectoryCreator(/* args */);
+  ~TrajectoryCreator();
+  void init();
+  void exec(path_struct &base_path, vector<trajectory_point_t> &trajectory);
+  void exec2(path_struct &base_path, vector<trajectory_point_t> &trajectory);
+  void exec3(path_struct &base_path, vector<trajectory_point_t> &trajectory);
+  void make_chopped_trajectory(vector<trajectory_point_t> &trajectory,
+                               int turn_num, bool dia);
 
-    TurnType get_turn_type(int turn_num) {
-        if (turn_num == 1  || turn_num == 2)  return TurnType::Normal;
-        if (turn_num == 3  || turn_num == 4)  return TurnType::Orval;
-        if (turn_num == 5  || turn_num == 6)  return TurnType::Large;
-        if (turn_num == 7  || turn_num == 8)  return TurnType::Dia45;
-        if (turn_num == 9  || turn_num == 10) return TurnType::Dia135;
-        if (turn_num == 11 || turn_num == 12) return TurnType::Dia90;
-        if (turn_num == 13 || turn_num == 14) return TurnType::Kojima;
-        if (turn_num == 255)                   return TurnType::Finish;
-        return TurnType::Finish;
-    }
+  Direction get_next_dir(Direction dir, TurnType type, TurnDirection turn_dir);
+  TurnType get_turn_type(int turn_num);
+  TurnType get_turn_type(int turn_num, bool dia);
+  string get_turn_type_string(int turn_num, bool dia);
+  string get_turn_dir_string(int turn_num);
 
-    TurnType get_turn_type(int turn_num, bool dia) {
-        if (turn_num == 1  || turn_num == 2)  return TurnType::Normal;
-        if (turn_num == 3  || turn_num == 4)  return TurnType::Orval;
-        if (turn_num == 5  || turn_num == 6)  return TurnType::Large;
-        if (turn_num == 7  || turn_num == 8)  return !dia ? TurnType::Dia45   : TurnType::Dia45_2;
-        if (turn_num == 9  || turn_num == 10) return !dia ? TurnType::Dia135  : TurnType::Dia135_2;
-        if (turn_num == 11 || turn_num == 12) return TurnType::Dia90;
-        if (turn_num == 13 || turn_num == 14) return TurnType::Kojima;
-        if (turn_num == 255)                   return TurnType::Finish;
-        return TurnType::Finish;
-    }
-
-    Direction get_next_dir(Direction dir, TurnType type, TurnDirection turn_dir) {
-        if (type == TurnType::Normal || type == TurnType::Large) {
-            if (dir == Direction::North)
-                return turn_dir == TurnDirection::Right ? Direction::East  : Direction::West;
-            if (dir == Direction::East)
-                return turn_dir == TurnDirection::Right ? Direction::South : Direction::North;
-            if (dir == Direction::West)
-                return turn_dir == TurnDirection::Right ? Direction::North : Direction::South;
-            if (dir == Direction::South)
-                return turn_dir == TurnDirection::Right ? Direction::West  : Direction::East;
-        } else if (type == TurnType::Orval) {
-            if (dir == Direction::North) return Direction::South;
-            if (dir == Direction::East)  return Direction::West;
-            if (dir == Direction::West)  return Direction::East;
-            if (dir == Direction::South) return Direction::North;
-        } else if (type == TurnType::Dia45) {
-            if (dir == Direction::North)
-                return turn_dir == TurnDirection::Right ? Direction::NorthEast : Direction::NorthWest;
-            if (dir == Direction::East)
-                return turn_dir == TurnDirection::Right ? Direction::SouthEast : Direction::NorthEast;
-            if (dir == Direction::West)
-                return turn_dir == TurnDirection::Right ? Direction::NorthWest : Direction::SouthWest;
-            if (dir == Direction::South)
-                return turn_dir == TurnDirection::Right ? Direction::SouthWest : Direction::SouthEast;
-        } else if (type == TurnType::Dia45_2) {
-            if (dir == Direction::NorthEast)
-                return turn_dir == TurnDirection::Right ? Direction::East  : Direction::North;
-            if (dir == Direction::NorthWest)
-                return turn_dir == TurnDirection::Right ? Direction::North : Direction::West;
-            if (dir == Direction::SouthEast)
-                return turn_dir == TurnDirection::Right ? Direction::South : Direction::East;
-            if (dir == Direction::SouthWest)
-                return turn_dir == TurnDirection::Right ? Direction::West  : Direction::South;
-        } else if (type == TurnType::Dia135) {
-            if (dir == Direction::North)
-                return turn_dir == TurnDirection::Right ? Direction::SouthEast : Direction::SouthWest;
-            if (dir == Direction::East)
-                return turn_dir == TurnDirection::Right ? Direction::SouthWest : Direction::NorthWest;
-            if (dir == Direction::West)
-                return turn_dir == TurnDirection::Right ? Direction::NorthEast : Direction::SouthEast;
-            if (dir == Direction::South)
-                return turn_dir == TurnDirection::Right ? Direction::NorthWest : Direction::NorthEast;
-        } else if (type == TurnType::Dia135_2) {
-            if (dir == Direction::NorthEast)
-                return turn_dir == TurnDirection::Right ? Direction::South : Direction::West;
-            if (dir == Direction::NorthWest)
-                return turn_dir == TurnDirection::Right ? Direction::East  : Direction::South;
-            if (dir == Direction::SouthEast)
-                return turn_dir == TurnDirection::Right ? Direction::West  : Direction::North;
-            if (dir == Direction::SouthWest)
-                return turn_dir == TurnDirection::Right ? Direction::North : Direction::East;
-        } else if (type == TurnType::Dia90) {
-            if (dir == Direction::NorthEast)
-                return turn_dir == TurnDirection::Right ? Direction::SouthEast : Direction::NorthWest;
-            if (dir == Direction::NorthWest)
-                return turn_dir == TurnDirection::Right ? Direction::NorthEast : Direction::SouthWest;
-            if (dir == Direction::SouthEast)
-                return turn_dir == TurnDirection::Right ? Direction::SouthWest : Direction::NorthEast;
-            if (dir == Direction::SouthWest)
-                return turn_dir == TurnDirection::Right ? Direction::NorthWest : Direction::SouthEast;
-        }
-        return dir;
-    }
+  TurnDirection get_turn_dir(int turn_dir);
+  double chop_dt = 3;
+  slalom_data_t sla_data;
 };
+
+#endif
