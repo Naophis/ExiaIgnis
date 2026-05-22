c1a4fd00f97d4a9f23372839095cfec893f991cf
Naophis <nao1288stusj@gmail.com>
2026-05-23 02:49:40 +0900

    refactor: replace file sending commands with picotool load command in write.sh

--- write.sh
+++ write.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-python3 send_file.py write tools/param_tuner/profile/hardware.yaml
-# python3 send_file.py write tools/param_tuner/profile/hf/profiles.yaml
-# python3 send_file.py write tools/param_tuner/profile/hf/run_prf.yaml
-python3 send_file.py write tools/param_tuner/profile/hf/sensor.yaml
-# python3 send_file.py write tools/param_tuner/profile/hf/offset.yaml
-# python3 send_file.py write tools/param_tuner/profile/hf/vel_prof.yaml
-# python3 send_file.py write tools/param_tuner/profile/system.yaml
+sudo ~/.local/bin/picotool load -f build/ExiaIgnis.uf2 -x
